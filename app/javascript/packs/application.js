826539e1e799c938a8202fda10d55f067ec641b2
Sebastian Jeong <sebastianrcnt@gmail.com>
2022-07-01 02:05:12 +0900

    :sparkles: feat: update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,15 +2,16 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require materialize
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
 
-require("trix")
-require("@rails/actiontext")
+require("trix");
+require("@rails/actiontext");
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,21 +1,35 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: [
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
+    // colors: {
+    //   current: "currentColor",
+    //   primary: "#00b8a9",
+    //   white: "#fff",
+    //   transparent: "transparent",
+    //   current: "currentColor",
+    //   black: colors.black,
+    //   white: colors.white,
+    //   gray: colors.gray,
+    //   emerald: colors.emerald,
+    //   indigo: colors.indigo,
+    //   yellow: colors.yellow,
+    // },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ]
-}
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+  ],
+};
