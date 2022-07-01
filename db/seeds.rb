a6cddfedea092e647ebe27e86b31c30f8b172ed3
Sebastian Jeong <sebastianrcnt@gmail.com>
2022-07-01 20:33:55 +0900

    fix: fix image bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding Database..."
+
+@user = User.create(
+    email: "sebastianrcnt@gmail.com",
+    name: "Sebastian Jeong",
+    password_digest: "none"
+)
+
+@product = Product.create(
+    name: "제주도 항공권 2매",
+    price: 12500,
+    owner: @user,
+    call_to_action: "탑승하러 가기"
+)
+
+puts "Seeding Complete!"
